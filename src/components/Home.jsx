43c61764e10a9abedd985cde5be49282177943ea
Saransh Gangwar <saransh7919@gmail.com>
2023-04-18 20:57:55 +0530

    updating home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Header from './Header'
 const Home = () => {
-  const location = useLocation()
   return (
     <div>
       <Navbar />
